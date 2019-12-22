af59094ee050039d5d0b0893525b0469c90315c9
Vasyl_Petrashchuk <rockeman2013@gmail.com>
2019-12-22 15:20:15 +0200

    Updated Many-To-Many

--- application/src/main/java/com/softserve/lv460/application/entity/Home.java
+++ application/src/main/java/com/softserve/lv460/application/entity/Home.java
@@ -16,10 +16,10 @@ public class Home {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String country;
   private String city;
-
   private String addressa;
+  @ManyToMany
+  private List<User> users;
 
 }
--- application/src/main/java/com/softserve/lv460/application/entity/User.java
+++ application/src/main/java/com/softserve/lv460/application/entity/User.java
@@ -7,9 +7,11 @@ import org.springframework.data.annotation.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @Entity
@@ -22,5 +24,6 @@ public class User {
   @NotNull
   @Size(min=8, message="Password must be at least 8 characters long")
   private String password;
-//private List<Home> homes;
+@ManyToMany
+  private List<Home> homes;
 }
