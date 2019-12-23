e1bdf9733e8b0a1ccb9e13900242e36872aba628
Olha Yaremchuk <olga7325@gmail.com>
2019-12-24 01:27:25 +0200

    adding feature (entity, services, dto)

---
+++ application/src/main/java/com/softserve/lv460/application/dto/home/FeatureRequest.java
@@ -0,0 +1,9 @@
+package com.softserve.lv460.application.dto.home;
+
+import lombok.Data;
+
+@Data
+public class FeatureRequest {
+  private String name;
+  private String description;
+}
--- application/src/main/java/com/softserve/lv460/application/service/FeatureService.java
+++ application/src/main/java/com/softserve/lv460/application/service/FeatureService.java
@@ -1,4 +1,14 @@
 package com.softserve.lv460.application.service;
 
+import com.softserve.lv460.application.repository.FeatureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeatureService {
+  @Autowired
+  private FeatureRepository featureRepository;
+
+  public FeatureRepository create()
+
 }
