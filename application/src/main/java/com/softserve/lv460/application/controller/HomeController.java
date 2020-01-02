a4911b8f16ada785c24a6332a5bda147d3722b5f
Ostap Melnychuk <ostapss222@gmail.com>
2020-01-03 00:05:19 +0200

    ssss

--- application/src/main/java/com/softserve/lv460/application/controller/HomeController.java
+++ application/src/main/java/com/softserve/lv460/application/controller/HomeController.java
@@ -2,17 +2,10 @@ package com.softserve.lv460.application.controller;
 
 import com.softserve.lv460.application.dto.home.HomeRequest;
 import com.softserve.lv460.application.dto.home.HomeResponse;
-import com.softserve.lv460.application.entity.Home;
 import com.softserve.lv460.application.service.HomeService;
-<<<<<<< HEAD
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
->>>>>>> 5a7092ed... dashboard added, home search via address added
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/homes")
@@ -45,20 +38,8 @@ public class HomeController {
     homeService.delete(id);
   }
 
-<<<<<<< HEAD
   @GetMapping("/{home_id}")
   public HomeResponse findOne(@PathVariable("home_id") Long id) {
     return homeService.findOneResponse(id);
-=======
-
-  @GetMapping("/find")
-  public Home findByAddress(@RequestParam String address, Model model) {
-    try {
-      return homeService.findHomeByAddress(address);
-    } catch (IllegalArgumentException e) {
-      model.addAttribute("error-message", "There is no such home for address " + address);
-    }
-    return new Home();
->>>>>>> 5a7092ed... dashboard added, home search via address added
   }
 }
