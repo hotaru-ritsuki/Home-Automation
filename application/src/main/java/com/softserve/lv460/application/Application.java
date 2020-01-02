7c488c79a484a00fa41a74d653c1ca69cd713444
Olha Yaremchuk <olga7325@gmail.com>
2020-01-02 19:01:41 +0200

    removed comments

--- application/src/main/java/com/softserve/lv460/application/Application.java
+++ application/src/main/java/com/softserve/lv460/application/Application.java
@@ -1,6 +1,5 @@
 package com.softserve.lv460.application;
 
-//import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
-
     SpringApplication.run(Application.class, args);
-
   }
-
 }
