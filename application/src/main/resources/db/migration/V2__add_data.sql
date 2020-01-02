a24a9b9a2582335142dcce578d598da3b85fe867
Olha Yaremchuk <olga7325@gmail.com>
2020-01-02 19:11:56 +0200

    insertion fixed

--- application/src/main/resources/db/migration/V2__add_data.sql
+++ application/src/main/resources/db/migration/V2__add_data.sql
@@ -1,32 +1,32 @@
-insert into users (id, email, password)
-  values (1, 'olga7325@gmail.com', '123');
+insert into users (email, password)
+  values ('olga7325@gmail.com', '123');
 
-insert into users (id, email, password)
-  values (2, 'vasya@gmail.com', '1111');
+insert into users (email, password)
+  values ('vasya@gmail.com', '1111');
 
-insert into home (id, addressa, city, country)
-  values (1, 'Rynok sqr 5', 'Lviv', 'Ukraine');
+insert into home (addressa, city, country)
+  values ('Rynok sqr 5', 'Lviv', 'Ukraine');
 
-insert into home (id, addressa, city, country)
-  values (2, 'Valova str 11', 'Lviv', 'Ukraine');
+insert into home (addressa, city, country)
+  values ('Valova str 11', 'Lviv', 'Ukraine');
 
-insert into location (id, name, home_id)
-  values (1, 'hall', 1);
+insert into location (name, home_id)
+  values ('hall', 1);
 
-insert into location (id, name, home_id)
-  values (2, 'living room', 1);
+insert into location (name, home_id)
+  values ('living room', 1);
 
-insert into location (id, name, home_id)
-  values (3, 'bedroom', 1);
+insert into location (name, home_id)
+  values ('bedroom', 1);
 
-insert into location (id, name, home_id)
-  values (4, 'hall', 2);
+insert into location (name, home_id)
+  values ('hall', 2);
 
-insert into location (id, name, home_id)
-  values (5, 'kitchen', 2);
+insert into location (name, home_id)
+  values ('kitchen', 2);
 
-insert into location (id, name, home_id)
-  values (6, 'bedroom', 2);
+insert into location (name, home_id)
+  values ('bedroom', 2);
 
 insert into user_home (user_id, home_id)
   values (1, 2);
