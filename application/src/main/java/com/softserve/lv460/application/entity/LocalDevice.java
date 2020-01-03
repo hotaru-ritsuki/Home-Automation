062b93de93e5e11edac62ad94a5fe4602b61bee7
Ostap Melnychuk <ostapss222@gmail.com>
2020-01-03 08:43:27 +0200

    done

--- application/src/main/java/com/softserve/lv460/application/entity/LocalDevice.java
+++ application/src/main/java/com/softserve/lv460/application/entity/LocalDevice.java
@@ -3,27 +3,25 @@ package com.softserve.lv460.application.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LocalDevice {
-    @Id
-    private String uuid;
+  @Id
+  private String uuid;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "location_id")
-    @NotNull
-    private Location locations;
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "location_id")
+  @NotNull
+  private Location locations;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "device_id")
-    @NotNull
-    private DeviceTemplate supportedDevice;
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "device_id")
+  @NotNull
+  private DeviceTemplate supportedDevice;
 }
