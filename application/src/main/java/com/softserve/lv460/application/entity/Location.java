d1acc7e9aa05392c2e41f6b60dabe0d180ce0c92
Alex Radyuk <43000799+AlexHack14@users.noreply.github.com>
2020-01-10 13:13:24 +0200

    Revert "Feature/dashboard"

--- application/src/main/java/com/softserve/lv460/application/entity/Location.java
+++ application/src/main/java/com/softserve/lv460/application/entity/Location.java
@@ -1,10 +1,8 @@
 package com.softserve.lv460.application.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -16,12 +14,4 @@ public class Location {
   private String name;
   @ManyToOne
   private Home home;
-
-  @ManyToMany(fetch = FetchType.LAZY)
-  @JsonIgnore
-  @JoinTable(name = "local_device",
-    joinColumns = @JoinColumn(name = "location_id"),
-    inverseJoinColumns = @JoinColumn(name = "device_id"))
-  private List<Feature> devices;
-
 }
