75e103fe584fabc0c7f277c53d9dfa94547967a1
Olha Yaremchuk <olga7325@gmail.com>
2020-01-26 16:15:32 +0200

    fixed controller and Password dto

--- application/src/main/java/com/softserve/lv460/application/security/dto/UpdPasswordRequest.java
+++ application/src/main/java/com/softserve/lv460/application/security/dto/UpdPasswordRequest.java
@@ -3,6 +3,7 @@ package com.softserve.lv460.application.security.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotBlank;
 
@@ -10,11 +11,11 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdPasswordRequest {
+  @Id
+  @NotBlank
+  private Integer id;
   @NotBlank
   private String currentPassword;
   @NotBlank
   private String password;
-  @NotBlank
-  private String confirmPassword;
-
 }
