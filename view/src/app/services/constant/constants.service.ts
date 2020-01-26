d1f1cc763327602e580ec0fc8fcc286d5c6c2e1c
Misha Slivisnkiy <miwka0806@gmail.com>
2020-01-26 13:38:33 +0200

    add some stuff for deploy

--- view/src/app/services/constant/constants.service.ts
+++ view/src/app/services/constant/constants.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  readonly baseApplicationUrl: string =  'https://rocky-dawn-44081.herokuapp.comheroku ';
+  readonly baseApplicationUrl: string =  'https://rocky-dawn-44081.herokuapp.com';
   readonly baseDeviceUrl: string = 'http://localhost:8081';
 }
