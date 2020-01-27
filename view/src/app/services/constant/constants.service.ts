14d83b665adcbe19b0e0488121f0c2ceaf0757f4
Misha Slivisnkiy <miwka0806@gmail.com>
2020-01-27 16:56:00 +0200

    final to deploy

--- view/src/app/services/constant/constants.service.ts
+++ view/src/app/services/constant/constants.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  readonly baseApplicationUrl: string =  'https://auotomation-application.herokuapp.comheroku';
+  readonly baseApplicationUrl: string =  'https://auotomation-application.herokuapp.com';
   readonly baseDeviceUrl: string = 'http://localhost:8081';
 }
