20f8e34a6c14bc9f09a19c8a3e956415cf39b12e
Ostap Melnychuk <ostapss222@gmail.com>
2020-01-27 20:03:18 +0200

    Frontend done

--- application/src/main/resources/db/migration/V2__add_data.sql
+++ application/src/main/resources/db/migration/V2__add_data.sql
@@ -25,7 +25,16 @@ INSERT INTO `user_home` (`user_id`, `home_id`) VALUES ('1', (SELECT @rynok)),
 
 INSERT INTO `device_template` (`id`, `brand`, `model`, `power_supply`, `release_year`, `type`)
     VALUES (1, 'Xiaomi', 'NUN4013CN', 'linear regulated', 2018, 'Humidity Meter'),
-            (2, 'Xiaomi', 'Mijia 2 Digital Hygrometer', 'unregulated', 2019, 'Temperature Meter');
+           (2, 'Xiaomi', 'Mijia 2 Digital Hygrometer', 'unregulated', 2019, 'Temperature Meter'),
+           (3, 'Aqara', 'WSDCGQ11LM', 'unregulated', 2017, 'Temperature Meter'),
+           (4, 'Life', 'TP-3', 'regulated', 2013, 'Temperature Meter'),
+           (5, 'ORVIBO', 'ZigBee (ST20-O)', 'regulated', 2018, 'Humidity Meter'),
+           (6, 'Aqara', 'AS008CNW01', 'regulated', 2017, 'Temperature Meter'),
+           (7, 'Ajax', '000005637', 'unregulated', 2016, 'Temperature Meter'),
+           (8, 'Ajax', '000005636', 'regulated', 2016, 'Humidity Meter'),
+           (9, 'Fibaro', 'FGMS-001', 'regulated', 2015, 'Temperature Meter'),
+           (10, 'Arton', 'TPT-2', 'regulated', 2014, 'Temperature Meter'),
+           (11, 'SATEL', 'ATD-100', 'regulated', 2013, 'Temperature Meter');
 
 INSERT INTO `local_device` (`uuid`, `location_id`, `supported_device_id`)
     VALUES ('1ec3cf2a-2a3b-11ea', (SELECT @locationHall), 1),
@@ -37,7 +46,20 @@ INSERT INTO `features` (`id`, `description`, `name`)
             (2, 'Humidity', 'getHumidity');
 
 INSERT INTO `device_features` (`device_id`, `features_id`, `specification`)
-  VALUES (1, 2, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}');
+  VALUES (1, 2, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (2, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (3, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (3, 2, '{"columnName": "humidity", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (4, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (5, 2, '{"columnName": "humidity", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (6, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (6, 2, '{"columnName": "humidity", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (7, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (8, 2, '{"columnName": "humidity", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (9, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (10, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (10, 2, '{"columnName": "humidity", "type": "numeric", "restriction": {"min": 15, "max": 25}}'),
+   (11, 1, '{"columnName": "temperature", "type": "numeric", "restriction": {"min": 15, "max": 25}}');
 
 INSERT INTO `actions` (`description`, `type`)
   VALUES ('send mail',1), ('send telegram',1);
