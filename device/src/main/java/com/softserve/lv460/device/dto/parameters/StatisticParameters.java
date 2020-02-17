7f4b285e96a95fec6457c29f09eb44814535494c
PavloBaluh <46240224+PavloBaluh@users.noreply.github.com>
2020-02-17 14:59:10 +0200

    Update StatisticParameters.java

--- device/src/main/java/com/softserve/lv460/device/dto/parameters/StatisticParameters.java
+++ device/src/main/java/com/softserve/lv460/device/dto/parameters/StatisticParameters.java
@@ -16,10 +16,10 @@ import javax.validation.constraints.Positive;
 public class StatisticParameters {
   @NotBlank(message = ValidationConstants.EMPTY_STATISTIC_TYPE)
   private String type;
-  @Pattern(regexp = "[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-1][0-9]:[0-1][0-9]:[0-1][0-9]\\+01:00",
+  @Pattern(regexp = "[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-1][0-9]:[0-6][0-9]:[0-6][0-9]\\+01:00",
           message = ValidationConstants.INVALID_FORMAT_DATE_FORMAT)
   private String from;
-  @Pattern(regexp = "[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-1][0-9]:[0-1][0-9]:[0-1][0-9]\\+01:00",
+  @Pattern(regexp = "[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-1][0-9]:[0-6][0-9]:[0-6][0-9]\\+01:00",
           message = ValidationConstants.INVALID_FORMAT_DATE_FORMAT)
   private String to;
   @NotNull(message = ValidationConstants.NULL_ID)
